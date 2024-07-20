d4be96b3388cd16b68621eee34aafa8b402cdf00
jeonchansu <bingryun@gmail.com>
2024-07-21 04:30:23 +0900

    fct_medm_reg api to s3 save

---
+++ dag/fct_medm_reg.py
@@ -0,0 +1,123 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.hooks.S3_hook import S3Hook
+from datetime import datetime, timedelta
+import requests
+import boto3
+import csv
+from io import StringIO
+import pendulum
+
+kst = pendulum.timezone("Asia/Seoul")
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,  # 선행작업의존여부N
+    'start_date': datetime(2024, 7, 21, 9, 0, 0, tzinfo=kst),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+def fct_medm_reg_to_s3(**kwargs):
+    api_url = "https://apihub.kma.go.kr/api/typ01/url/fct_medm_reg.php?"
+    api_key = "HGbLr74hS2qmy6--ITtqog"
+
+    params = {
+        'mode': 1,
+        'disp': 1,
+        'help': 0,
+        'authKey': api_key
+    }
+
+    response = requests.get(api_url, params=params)
+
+    if response.status_code == 200:
+        response_text = response.text
+        
+        if "#START7777" in response_text and "#7777END" in response_text:
+            lines = response_text.splitlines()
+            area_data = []
+            
+            start_index = 0
+            end_index = len(lines)
+            
+            for i, line in enumerate(lines):
+                if line.startswith('# REG_ID'):
+                    start_index = i + 1
+                elif line.startswith('#7777END'):
+                    end_index = i
+                    break
+            
+            for line in lines[start_index:end_index]:
+                if line.strip():
+                    columns = line.split()
+                    if len(columns) >= 5:
+                        try:
+                            reg_id = columns[0]
+                            tm_st = datetime.strptime(columns[1], '%Y%m%d%H%M')
+                            tm_ed = datetime.strptime(columns[2], '%Y%m%d%H%M')
+                            reg_sp = columns[3]
+                            reg_name = ' '.join(columns[4:])
+                            area_data.append((reg_id, tm_st, tm_ed, reg_sp, reg_name))
+                        except ValueError as e:
+                            print(f"WARN : 잘못된 날짜 형식 - {line}")
+                    else:
+                        print(f"WARN : 라인 데이터 부족 - {line}")
+            
+            if area_data:
+                # s3 버킷 디렉토리 생성 기준을 tm_st 기준으로
+                max_tm_st = max(area_data, key=lambda x: x[1])[1]
+                year = max_tm_st.strftime('%Y')
+                month = max_tm_st.strftime('%m')
+                day = max_tm_st.strftime('%d')
+                formatted_date = max_tm_st.strftime('%Y_%m_%d')
+
+                csv_buffer = StringIO()
+                csv_writer = csv.writer(csv_buffer)
+                csv_writer.writerow(['REG_ID', 'TM_ST', 'TM_ED', 'REG_SP', 'REG_NAME'])
+                csv_writer.writerows(area_data)
+                
+                s3_hook = S3Hook(aws_conn_id='AWS_S3')
+                bucket_name = 'team-okky-1-bucket'
+                s3_key = f'fct_medm_reg/{year}/{month}/{day}/{formatted_date}_fct_medm_reg.csv'
+                
+                try:
+                    s3_hook.load_string(
+                        csv_buffer.getvalue(),
+                        key=s3_key,
+                        bucket_name=bucket_name,
+                        replace=True
+                    )
+                    print("저장성공")
+                except Exception as e:
+                    print(f"S3 업로드 실패: {e}")
+                    raise ValueError(f"S3 업로드 실패: {e}")
+            else:
+                print("No valid data to insert.")
+                raise ValueError("No valid data to insert.")
+        else:
+            print("저장실패")
+            print("오류메세지:", response_text)
+            raise ValueError(f"저장실패: 오류메세지: {response_text}")
+    else:
+        print(f"응답코드오류: {response.status_code}")
+        print("메세지:", response.text)
+        raise ValueError(f"응답코드오류: {response.status_code}, 메세지: {response.text}")
+
+with DAG(
+    'fct_medm_reg_to_s3',
+    default_args=default_args,
+    description='fct_medm_reg upload to S3',
+    schedule_interval='0 9 * * *',
+    catchup=False,
+) as dag:
+    dag.timezone = kst
+    
+    fct_medm_reg_to_s3_task = PythonOperator(
+        task_id='fct_medm_reg_to_s3',
+        python_callable=fct_medm_reg_to_s3,
+    )
+
+    fct_medm_reg_to_s3_task
